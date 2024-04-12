7eda4e9e83e1831b2fdbd4afd8c7b989f24762dc
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-12 19:57:29 -0300

    Adiciona rota para a classe Bancos em api_revgas/urls.py

--- revgasAPI/urls.py
+++ revgasAPI/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from api_revgas.views import BancoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'bancos', BancoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
