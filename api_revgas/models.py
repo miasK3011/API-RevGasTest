b245386a4c4523d78452a05bfb2bbcd9f3292207
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-12 17:14:39 -0300

    first commit

---
+++ api_revgas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Bancos(models.Model):
+    # Colunas codigo e instituicao
+    codigo = models.CharField(max_length=100, primary_key=True)
+    instituicao = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.instituicao
