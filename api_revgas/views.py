abae266c49c0744bf18692b0026fdd8205e01114
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-12 19:56:10 -0300

    Adiciona viewset para a classe Bancos em api_revgas/views.py

--- api_revgas/views.py
+++ api_revgas/views.py
@@ -1,3 +1,17 @@
+from django.http import Http404
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
+from .models import Bancos
+from .serializers import BancoSerializer
 
-# Create your views here.
+class BancoViewSet(viewsets.ModelViewSet):
+    queryset = Bancos.objects.all()
+    serializer_class = BancoSerializer
+    lookup_field = 'codigo'
+    
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            raise NotFound("Banco não encontrado com o código fornecido.")
